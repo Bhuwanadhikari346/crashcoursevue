ad36243ba5927ef722640e83df05f66a0206bc07
Bhuwan Adhikari <bhuwanadhikari54@gmail.com>
2022-03-03 18:51:31 +0545

    without json server

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="onClickHandler()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+</template>
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClickHandler() {
+      this.$emit("btn-click");
+    },
+  },
+};
+</script>
